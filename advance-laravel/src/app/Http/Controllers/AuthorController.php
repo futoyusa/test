6f03f378c6c0acc19d712e2b596e2a54dd4db7bc
futo yusa <futo08yuto@outlook.jp>
2024-05-27 17:42:26 +0900

    message

--- advance-laravel/src/app/Http/Controllers/AuthorController.php
+++ advance-laravel/src/app/Http/Controllers/AuthorController.php
@@ -12,35 +12,21 @@ class AuthorController extends Controller
         $authors = Author::all();
         return view('index', ['authors' => $authors]);
     }
-    public function add(){
-        return view('add');
+    public function find(){
+        return view('find', ['input' => '']);
     }
-    public function create(Request $request){
-        $form = $request->all();
-        Author::create($form);
-        return redirect('/');
-    }
-
-    public function edit(Request $request){
-        $author = Author::find($request->id);
-        return view('edit', ['form' => $author]);
-    }
-
-    public function update(Request $request)
-    {
-        $form = $request->all();
-        unset($form['_token']);
-        Author::find($request->id)->update($form);
-        return redirect('/');
-    }
-    public function delete(Request $request)
-    {
-        $author = Author::find($request->id);
-        return view('delete', ['author' => $author]);
-    }
-    public function remove(Request $request)
-    {
-        Author::find($request->id)->delete();
-        return redirect('/');
-    }
+    public function search(Request $request){
+        $item = Author::where('name', $request->input)->first();
+        $param = [
+            'input' => $request->input,
+            'item' => $item
+            ];
+            return view('find', $param);
+            }
+        public function bind(Author $author){
+            $data = [
+            'item'=>$author,
+            ];
+            return view('author.binds', $data);
+        }
 }
--- advance-laravel/src/routes/web.php
+++ advance-laravel/src/routes/web.php
@@ -12,10 +12,6 @@ use App\Http\Controllers\AuthorController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [AuthorController::class, 'index']);
-Route::get('/add', [AuthorController::class, 'add']);
-Route::post('/add', [AuthorController::class, 'create']);
-Route::get('/edit', [AuthorController::class, 'edit']);
-Route::post('/edit', [AuthorController::class, 'update']);
-Route::get('/delete', [AuthorController::class, 'delete']);
-Route::post('/delete', [AuthorController::class, 'remove']);
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
+Route::get('/author/{author}', [AuthorController::class, 'bind']);
