c2d32216c100285528bf81eefb4d7dae959e9427
futo yusa <futo08yuto@outlook.jp>
2024-05-29 17:52:48 +0900

    message

--- advance-laravel/src/app/Http/Controllers/AuthorController.php
+++ advance-laravel/src/app/Http/Controllers/AuthorController.php
@@ -4,29 +4,59 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Author;
-
+// フォームリクエストの読み込み
+use App\Http\Requests\AuthorRequest;
 
 class AuthorController extends Controller
 {
-    public function index(){
+    // データ一覧ページの表示
+    public function index()
+    {
         $authors = Author::all();
         return view('index', ['authors' => $authors]);
+   }
+
+   // データ追加用ページの表示
+    public function add()
+    {
+        return view('add');
     }
-    public function find(){
-        return view('find', ['input' => '']);
+
+    // 追加機能
+    public function create(AuthorRequest $request)
+    {
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
     }
-    public function search(Request $request){
-        $item = Author::where('name', $request->input)->first();
-        $param = [
-            'input' => $request->input,
-            'item' => $item
-            ];
-            return view('find', $param);
-            }
-        public function bind(Author $author){
-            $data = [
-            'item'=>$author,
-            ];
-            return view('author.binds', $data);
-        }
+
+    // データ編集ページの表示
+    public function edit(Request $request){
+        $author = Author::find($request->id);
+        return view('edit', ['form' => $author]);
+    }
+
+    // 更新機能
+    public function update(Request $request)
+    {
+        $form = $request->all();
+        unset($form['_token']);
+        Author::find($request->id)->update($form);
+        return redirect('/');
+    }
+
+    // データ削除用ページの表示
+    public function delete(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('delete', ['author' => $author]);
+    }
+
+    // 削除機能
+    public function remove(Request $request)
+    {
+        Author::find($request->id)->delete();
+        return redirect('/');
+    }
+
 }
--- advance-laravel/src/routes/web.php
+++ advance-laravel/src/routes/web.php
@@ -12,6 +12,13 @@ use App\Http\Controllers\AuthorController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [AuthorController::class, 'index']);
 Route::get('/find', [AuthorController::class, 'find']);
 Route::post('/find', [AuthorController::class, 'search']);
-Route::get('/author/{author}', [AuthorController::class, 'bind']);
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
+Route::get('/edit', [AuthorController::class, 'edit']);
+Route::post('/edit', [AuthorController::class, 'update']);
+Route::get('/delete', [AuthorController::class, 'delete']);
+Route::post('/delete', [AuthorController::class, 'remove']);
+Route::get('/verror', [AuthorController::class, 'verror']);
